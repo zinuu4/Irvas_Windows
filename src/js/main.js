4dbaf2cf24f9294e5c9ede25a603b70316cccfd7
zinuu4 <unvm7721@gmail.com>
2023-05-21 13:55:42 +0300

    implemented script for timer

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ import modals from './modules/modals';
 import tabs from './modules/tabs';
 import forms from './modules/forms';
 import changeModalState from './modules/changeModalState';
+import timer from './modules/timer';
 
 window.addEventListener('DOMContentLoaded', () => {
   'use strict';
@@ -15,4 +16,5 @@ window.addEventListener('DOMContentLoaded', () => {
   tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
   tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
   forms(modalState);
+  timer('.timer1', '2023-06-20');
 });
