400dfa741814977e6de1afc38acc5fe1828542ba
zinuu4 <unvm7721@gmail.com>
2023-05-21 14:55:13 +0300

    added script for showing images and and fixed small bugs

---
+++ src/js/modules/images.js
@@ -0,0 +1,38 @@
+const images = () => {
+  const imgPopup = document.createElement('div'),
+        workSection = document.querySelector('.works'),
+        bigImage = document.createElement('img');
+
+  imgPopup.classList.add('popupImg');
+  workSection.appendChild(imgPopup);
+
+  imgPopup.style.justifyContent = 'center';
+  imgPopup.style.alignItems = 'center';
+  imgPopup.style.display = 'none';
+
+  bigImage.style.width = '70%';
+  bigImage.style.height = '70%';
+
+  imgPopup.appendChild(bigImage);
+
+  workSection.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    let target = e.target;
+
+    if (target && target.classList.contains('preview')) {
+      imgPopup.style.display = 'flex';
+      document.body.style.overflow = 'hidden';
+
+      const path = target.parentNode.getAttribute('href');
+      bigImage.setAttribute('src', path);
+    }
+
+    if (target && target.matches('div.popupImg')) {
+      imgPopup.style.display = 'none';
+      document.body.style.overflow = '';
+    }
+  });
+};
+
+export default images;
--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -3,7 +3,8 @@ const modals = () => {
     const trigger = document.querySelectorAll(triggerSelector),
           modal = document.querySelector(modalSelector),
           close = document.querySelector(closeSelector),
-          windows = document.querySelectorAll('[data-modal]');
+          windows = document.querySelectorAll('[data-modal]'),
+          scroll = calcScroll();
 
     trigger.forEach(item => {
       item.addEventListener('click', (e) => {
@@ -17,6 +18,8 @@ const modals = () => {
   
         modal.style.display = 'block';
         document.body.style.overflow = 'hidden';
+
+        document.body.style.marginRight = `${scroll}px`;
       });
     });
 
@@ -27,6 +30,8 @@ const modals = () => {
 
       modal.style.display = 'none';
       document.body.style.overflow = '';
+
+      document.body.style.marginRight = `0px`;
     });
 
     modal.addEventListener('click', (e) => {
@@ -37,6 +42,7 @@ const modals = () => {
 
         modal.style.display = 'none';
         document.body.style.overflow = '';
+        document.body.style.marginRight = `0px`;
       }
     });
   }
@@ -47,13 +53,28 @@ const modals = () => {
       document.body.style.overflow = 'hidden';
     }, time);
   }
+
+  function calcScroll() {
+    let div = document.createElement('div');
+
+    div.style.width = '50px';
+    div.style.height = '50px';
+    div.style.overflowY = 'scroll';
+    div.style.visibility = 'hidden';
+
+    document.body.appendChild(div);
+    let scrollWidth = div.offsetWidth - div.clientWidth;
+    div.remove();
+
+    return scrollWidth;
+  }
   
   bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
   bindModal('.phone_link', '.popup', '.popup .popup_close');
   bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
   bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', 'false');
   bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', 'false');
-  // showModalByTime('.popup', 60000);
+  showModalByTime('.popup', 60000);
 };
 
 export default modals;
